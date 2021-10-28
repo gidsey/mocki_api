3320245543bcee26a4a9e555cbe34376bfae2a13
Chris Guy <chris@chrisguy.co>
2021-10-28 15:28:27 +0100

    30119-100-balenciaga endpint removeed

--- images/data_utils.py
+++ images/data_utils.py
@@ -8,15 +8,16 @@ def get_data_files():
     files = []
 
     for filename in os.listdir(DATA_PATH):
-        try:
-            print('filename', filename)
-            with open(os.path.join(DATA_PATH, filename)) as f:
-                json_data = json.loads(f.read())
-            files.append({
-                'filename': filename,
-                'data': json_data,
-            })
-        except json.decoder.JSONDecodeError as e:
-            print(f'Error in reading json file: {filename}. Error: {str(e)}')
+        if not filename.startswith('.'):
+            try:
+                print('filename', filename)
+                with open(os.path.join(DATA_PATH, filename)) as f:
+                    json_data = json.loads(f.read())
+                files.append({
+                    'filename': filename,
+                    'data': json_data,
+                })
+            except json.decoder.JSONDecodeError as e:
+                print(f'Error in reading json file: {filename}. Error: {str(e)}')
 
     return files
